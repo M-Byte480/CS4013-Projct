c5b94f593aa343cb8212f83d182f1a2fd0f199e1
tadhgryan2012 <tadhgryan2012@gmail.com>
2022-11-13 14:04:29 +0000

    Fixed Table

--- src/till/Table.java
+++ src/till/Table.java
@@ -1,19 +1,14 @@
 package till;
 
-import reservation.Invoice;
-import reservation.LineItem;
 import reservation.Reservation;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Table {
     private int tableNumber;
     private int seats;
     private Reservation booking;
     private ArrayList<Product> productsOnTable;
-    private Till till;
 
     public Table(int tableNumber, int seats) {
         this.tableNumber = tableNumber;
@@ -23,13 +18,24 @@ public class Table {
     public int getTableNumber() {
         return tableNumber;
     }
+    
+    public String getStaff() {
+        return getStaff();
+    }
 
-    //adding product to "order"
+    public Reservation getBooking() {
+        return booking;
+    }
+    public void setBooking(Reservation booking) {
+        this.booking = booking;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return productsOnTable;
+    }
     public void addProduct(Product pick) {
         productsOnTable.add(pick);
     }
-
-    //deleting product from "order"
     public void deleteProduct(Product delete) {
         productsOnTable.add(delete);
     }
@@ -40,60 +46,6 @@ public class Table {
             sum += p.getCost();
         }
         return sum ;
-    }
-
-    public String getStaff() {
-        return getStaff();          // uhh recursion
-    }
-    
-    // Converts table to lineItem arrayList for invoices
-    // Milan
-    public static ArrayList<LineItem> convertToLineItems(ArrayList<Product> products) {
-        ArrayList<LineItem> items = new ArrayList<>();
-        HashMap<Product, Integer> occurences = new HashMap<>();
-        Integer count = 0;
-        for (Product p : products) {
-            count = occurences.get(p);
-            if (count == null) {
-                occurences.put(p, 1);
-            } else {
-                occurences.replace(p, count + 1);
-            }
-        }
-
-        for (HashMap.Entry<Product, Integer> item : occurences.entrySet()) {
-            items.add(new LineItem(item.getKey().getName(), item.getValue(), item.getKey().getCost()));
-        }
-        return items;
-    }
-
-    public void closeTable() throws IOException {
-        Invoice invoice = new Invoice(this.booking);
-        invoice.sendInvoice();
-        this.booking = null;
-        this.productsOnTable = null;
-    }
-
-    //closing/deleting booking
-    //check if table is vacant
-    public void deleteTable(Table table) {
-        productsOnTable.remove(table);          // cant remove table object from arraylist of products
-    }
-
-    public String returnForawrdSlaah() {
-        for (Product p : productsOnTable) {
-            St
-            p.toString();                       // ummm what
-        }
-    }
-
-    public void getMenu(Table t) throws IOException {
-        Menu menu = new Menu();
-        menu.run(t);
-    }
-
-    public ArrayList<Product> getProducts() {
-        return productsOnTable;
     }
 
     public String toString() {
