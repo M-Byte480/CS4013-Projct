87d2f319a2f24c02e4a442e1a0333b6a19eadb9e
tadhgryan2012 <tadhgryan2012@gmail.com>
2022-11-18 15:05:25 +0000

    Revert "wont let me pull without committing"
    This reverts commit 0d4fea308b8c5b613598168fb1decd0d46068174.

--- src/till/Table.java
+++ src/till/Table.java
@@ -5,12 +5,7 @@ import java.util.ArrayList;
 public class Table {
     private int tableNumber;
     private int seats;
-    private Reservation booking;
-    private ArrayList<Product> productsOnTable;
-    private Till till;
-    private Staff staffServing;
-
-    //staff data field, not in constructor, setter and remove
+    private ArrayList<Product> products;
 
     public Table(int tableNumber, int seats) {
         this.tableNumber = tableNumber;
@@ -19,15 +14,6 @@ public class Table {
 
     public int getTableNumber() {
         return tableNumber;
-
-
-
-
-    //adding product to "order"
-    public void addProduct(Table x, Product pick) {
-
-        productsOnTable.add(pick);
-
     }
 
     public int getSeats() {
@@ -35,28 +21,6 @@ public class Table {
     }
     public void setSeats(int seats) {
         this.seats = seats;
-
-    // Converts table to lineItem arrayList for invoices
-    // Milan
-
-
-
-
-
-    public double getTotal(){
-        double sum = 0;
-        for (Product p : productsOnTable) {
-            sum += p.getCost();
-
-        }
-        return sum ;
-    }
-
-    //closing/deleting booking
-    //check if table is vacant
-    public void deleteTable(Table table) {
-        productsOnTable.remove(table);
-
     }
 
     public ArrayList<Product> getProducts() {
@@ -84,67 +48,3 @@ public class Table {
 	}
 }
 
-    public String getStaff() {
-        return getStaff();
-    }
-    public static ArrayList<LineItem> convertToLineItems(ArrayList<Product> products) {
-        ArrayList<LineItem> items = new ArrayList<>();
-        HashMap<Product, Integer> occurences = new HashMap<>();
-        Integer count = 0;
-        for (Product p : products) {
-            count = occurences.get(p);
-            if (count == null) {
-                occurences.put(p, 1);
-            } else {
-                occurences.replace(p, count + 1);
-                count = occurences.get(p.getName());
-                if (count == null) {
-                    occurences.put(p, 1);
-                } else {
-                    occurences.replace(p, count + 1);
-                }
-            }
-        }
-
-            for (HashMap.Entry<Product, Integer> item :
-                    occurences.entrySet()) {
-                items.add(new LineItem(item.getKey().getName(), item.getValue(), item.getKey().getCost()));
-            }
-
-            return items;
-        }
-
-
-
-
-        public void closeTable () throws IOException {
-            Invoice invoice = new Invoice(this.booking);
-            invoice.sendInvoice();
-            this.booking = null;
-            this.productsOnTable = null;
-        }
-
-        //closing/deleting booking
-        //check if table is vacant
-
-
-            public String returnForawrdSlaah(){ //Return all products in products array with delimiter '/'
-                for (Product p : productsOnTable) {
-                    St
-                    p.toString();
-                }
-                return "";
-            }
-
-            public void getMenu (Table t) throws IOException {
-                Menu menu = new Menu();
-                menu.run(t);
-            }
-
-
-
-
-        public void setStaffServing (Staff staffServing){
-            this.staffServing = staffServing;
-        }
-    }
